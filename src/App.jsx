f659b5c9e54fe333304ce78f940fa4a77832854d
Lasha Bulatsashvili <lashareichert@gmail.com>
2022-12-04 03:55:35 +0400

    feat: add project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AboutUs from "./views/AboutUs.jsx";
+
+function App() {
+  return (
+    <div className="App">
+        <AboutUs/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/views/AboutUs.jsx
@@ -0,0 +1,11 @@
+import AboutSection from "../components/AboutSection.jsx";
+
+const AboutUs = () => {
+  return(
+      <div>
+          <AboutSection/>
+      </div>
+  )
+}
+
+export default AboutUs
