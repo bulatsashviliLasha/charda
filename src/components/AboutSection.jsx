1c7a13f994b10fcab439701373803fe59faf92fd
Lasha Bulatsashvili <lashareichert@gmail.com>
2022-12-04 16:25:57 +0400

    style about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,24 +4,24 @@ import styled from "styled-components";
 const AboutSection = () => {
   return(
       <About>
-          <div className="description">
+          <Description>
               <div className="title">
-                  <div className="hide">
+                  <Hide>
                       <h2>We work to make</h2>
-                  </div>
-                  <div className="hide">
+                  </Hide>
+                  <Hide>
                       <h2>your <span>dreams</span> come</h2>
-                  </div>
-                  <div className="hide">
+                  </Hide>
+                  <Hide>
                       <h2>true.</h2>
-                  </div>
+                  </Hide>
               </div>
               <p>Contact us for any photography or videography ideas that you have.</p>
               <button>CONTACT US</button>
-          </div>
-          <div className="image">
+          </Description>
+          <Image>
               <img src={home1} alt="Guy with a camera"/>
-          </div>
+          </Image>
       </About>
   )
 };
@@ -33,6 +33,28 @@ const About = styled.div`
   justify-content: space-between;
   padding: 5rem 10rem;
   color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2{
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img{
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
 `;
 
 export default AboutSection;
